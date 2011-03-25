41deb8561856322f131bd32127fc0bde76a339b9
James Smith <james@floppy.org.uk>
2011-03-25 16:04:06 +0000

    Add generator for persistence setup. Includes addition of persistence method config file. MA-673

---
+++ lib/generators/persistence/persistence_generator.rb
@@ -0,0 +1,14 @@
+class PersistenceGenerator < Rails::Generator::Base
+  def manifest # this method is default entrance of generator
+    record do |m|
+      # Get method from command line - default is metadata
+      method = args[0] || 'metadata'
+      # Make sure there is a config directory
+      m.directory File.join("config")
+      # Create persistence.yml file
+      m.template File.join("config","persistence.yml.erb"), 
+                 File.join("config","persistence.yml"), 
+                 :assigns => {:method => method}
+    end
+  end
+end
