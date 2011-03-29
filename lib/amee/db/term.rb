74bac17412381ad1b4ebebae64865b663d073762
spatchcock <andrew.berkeley.is@googlemail.com>
2011-03-29 16:26:27 +0100

    integration fixes

--- lib/amee/db/term.rb
+++ lib/amee/db/term.rb
@@ -3,7 +3,7 @@ module AMEE
   module Db
     class Term < ActiveRecord::Base
 
-      belongs_to :calculation
+      belongs_to :calculation, :class_name => "AMEE::Db::Calculation"
       validates_presence_of :calculation_id, :label, :value
       before_save :initialize_value
 
