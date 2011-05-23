5439326f66e518bbab1fb86a6d309c7428af7738
George Palmer <george.palmer@gmail.com>
2011-05-23 15:18:44 +0100

    Remove use of Quantify in non-units persistence branch [AC-75]

--- lib/amee/db/term.rb
+++ lib/amee/db/term.rb
@@ -1,33 +1,17 @@
-
 module AMEE
   module Db
     class Term < ActiveRecord::Base
 
       belongs_to :calculation, :class_name => "AMEE::Db::Calculation"
       validates_presence_of :calculation_id, :label
-      before_save :initialize_value
 
       def update_value!(value)
         self.value = value
         save!
       end
 
-      def initialize_value
-        if value.is_a? Quantify::Quantity
-          self.value = value.to_s(:label)
-        else
-          self.value = value.to_s
-        end
-      end
-      
-      def value_or_quantity_object
-        Quantify::Quantity.parse value
-      rescue Quantify::QuantityParseError
-        value
-      end
-
       def to_hash
-        { label.to_sym => value_or_quantity_object }
+        { label.to_sym => value }
       end
 
     end
