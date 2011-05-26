b6cf690b217cda4ba3de4b26cdece84976ee4732
George Palmer <george.palmer@gmail.com>
2011-05-26 14:14:26 +0100

    Raise error if use include and then abuse the to use terms in conditions

--- lib/amee/data_abstraction/persistence_support.rb
+++ lib/amee/data_abstraction/persistence_support.rb
@@ -49,7 +49,8 @@ module AMEE
       module ClassMethods
 
         def find(*args)
-          args.last[:include] = "terms" if args.last[:joins].present?
+          raise ActiveRecord::ActiveRecordError.new("Using :include with terms and then conditioning on terms doesn't work due to rails caching.  Use the :joins option instead.") if args.last[:include].to_s.match(/terms/) && args.last[:conditions].to_s.match(/terms/)
+          args.last[:include] = "terms" if args.last[:joins].to_s.match(/terms/)
           result = AMEE::Db::Calculation.find(*args)
           return nil unless result
           if result.respond_to?(:map)
